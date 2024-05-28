758c170814b1516fe3aad0c50c77f1c3d9645463
cppmonger <keskinemirhan@gmail.com>
2024-05-28 20:43:31 +0300

    Add Typeorm for sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: "sqlite",
+    database: "db.sqlite",
+    entities: []
+  })],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
