8c7d441120a8cd9b543b4155c8eeea680a4a790a
cppmonger <keskinemirhan@gmail.com>
2024-05-28 20:51:11 +0300

    Add User Module and User Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: "sqlite",
     database: "db.sqlite",
-    entities: []
+    entities: [User]
   })],
   controllers: [],
   providers: [],
